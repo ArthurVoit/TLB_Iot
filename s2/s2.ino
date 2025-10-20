72ce4de30585523d1513dfea6d5c4db8e9546150
andre-axt <andreaxtbachtold@gmail.com>
2025-10-20 12:13:18 -0300

    Conexão com broker

--- s2/s2.ino
+++ s2/s2.ino
@@ -1,6 +1,17 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const int PORT = 1883;
+const String URL = "test.mosquitto.org";
+const String TOPIC = "DSM2";
+const String broker_user = "adnre"; 
+const String broker_pass = "ada"; 
 
 void setup() {
   Serial.begin(115200);
@@ -11,7 +22,16 @@ void setup() {
     delay(200);
   }
   Serial.println("\nConectado!");
-  WiFi.localIP();
+  Serial.println("Conectando ao Broker...");
+  mqtt.setServer(URL.c_str(), PORT);
+  while(!mqtt.connected()){
+    String ID = "S1-";
+    ID += String(random(0xffff), HEX);
+    mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str());
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("\n Conectado ao broker com sucesso!");
 }
 
 void loop() {
